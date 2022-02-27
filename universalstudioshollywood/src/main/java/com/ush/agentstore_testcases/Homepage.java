483e8f703952b80aaeead8dbe70712b225c3863b
gneshan <60718950+gneshan@users.noreply.github.com>
2022-02-27 13:13:27 -0800

    Adding home page to test case

---
+++ universalstudioshollywood/src/main/java/com/ush/agentstore_testcases/Homepage.java
@@ -0,0 +1,16 @@
+package com.ush.agentstore_testcases;
+
+import com.ush.agentstore_pageobjects.AgentStore_HomePage;
+
+public class Homepage extends ReUsableLibraries{
+
+	public void EnterDetailsOnHomePage() {
+	
+		
+		
+	}
+	
+	
+	
+	
+}
